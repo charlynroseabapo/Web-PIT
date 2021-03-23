619930b99c13a8a76d93fb1a1fbecc098ae881e7
charlyn <abapo.charlynrose@gmail.com>
2021-03-23 15:31:37 +0800

    mich try daw ni

--- TO DO LIST/js/script.js
+++ TO DO LIST/js/script.js
@@ -18,3 +18,31 @@ function todo(todotask,elmnt,color) {
   
   }
   document.getElementById("defaultOpen").click();
+
+//TO ADD LIST OF TASK
+  function newElement(){
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === ''){
+      alert ("Please input something!");
+    }else{
+      document.getElementById("myList").appendChild(li);
+    }
+      document.getElementById("myInput").value="";
+
+      var span = document.createElement("SPAN");
+      var txt = document.createTextNode("\u00D7");
+      span.className="close";
+      span.appendChild(txt);
+      li.appendChild(span);
+
+      for(i = 0; i<close.length; i++){
+        close [i].onclick = function(){
+          var div = this.parentElement;
+          div.style.display="none";
+        }
+      }
+    
+  }
